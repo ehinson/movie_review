2fad17b518e62c865b41053462eb99401d34d9a5
Erin Hinson <hinson.erin@gmail.com>
2015-01-19 21:53:09 -0500

    Sessions controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  
+  get '/register' to: 'users#new'
+  get '/login' to: 'sessions#new'
+
+  post '/login' to: 'sessions#create'
+
+  get '/logout' to: 'sessions#destroy'
+
   resources :users
 
   resources :movies
