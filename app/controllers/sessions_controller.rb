0a294695d178cff1bf27bf910dfe0313e3a1f689
Erin Hinson <hinson.erin@gmail.com>
2015-01-19 22:28:45 -0500

    Create login and registration.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,22 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(username: params[:username])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome, you've logged in"
+      redirect_to login_path
+
+    else
+      flash.now[:error] = "Never forget what you are, for surely the world will not. Make it your strength. Then it can never be your weakness. Armour yourself in it, and it will never be used to hurt you. Try to remember your correct username or password."
+      render :new
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Goodbye!"
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   
   get '/register', to: 'users#new'
+  
   get '/login', to: 'sessions#new'
 
   post '/login', to: 'sessions#create'
 
   get '/logout', to: 'sessions#destroy'
 
-  resources :users
+  resources :users, only: [:new, :create]
 
   resources :movies
 
