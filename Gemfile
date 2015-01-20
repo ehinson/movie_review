281efd18b3b4a082a0a66d2f8b4701455eb2232b
Erin Hinson <hinson.erin@gmail.com>
2015-01-19 22:02:21 -0500

    Install pry

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+
+gem 'pry'
 gem 'bcrypt-ruby', '~> 3.1.5'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.8'
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   
-  get '/register' to: 'users#new'
-  get '/login' to: 'sessions#new'
+  get '/register', to: 'users#new'
+  get '/login', to: 'sessions#new'
 
-  post '/login' to: 'sessions#create'
+  post '/login', to: 'sessions#create'
 
-  get '/logout' to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
 
   resources :users
 
