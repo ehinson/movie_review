da57f081203a0f146e80b78fdbc66c86dd1b5663
Erin Hinson <hinson.erin@gmail.com>
2015-01-19 21:42:27 -0500

    Add users. Install bcrypt.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150120003424) do
+ActiveRecord::Schema.define(version: 20150120021916) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20150120003424) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "password"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
