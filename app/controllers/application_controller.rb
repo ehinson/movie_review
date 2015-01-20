c3c13c8bab86e86dd03ef7bee9f63e11eb038833
Erin Hinson <hinson.erin@gmail.com>
2015-01-19 23:18:50 -0500

    Associate movies and users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :logged_in?
-  
+  helper_method :current_user, :logged_in?, :require_user
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
@@ -12,5 +12,11 @@ class ApplicationController < ActionController::Base
   def logged_in?
     !!current_user
   end
-
+  
+  def require_user
+    if !logged_in?
+      flash[:error]="You are not logged in."
+      redirect_to root_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   get '/register', to: 'users#new'
-  
+
   get '/login', to: 'sessions#new'
 
   post '/login', to: 'sessions#create'
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
 
   resources :movies
+
+  root to: "movies#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
