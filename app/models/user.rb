55763fd5f43b52bfe4b5e10dc2b4b22e65e1e839
Erin Hinson <hinson.erin@gmail.com>
2015-01-24 18:32:55 -0500

    Style and validate Sign-in, Log-in and Add movie forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
+  validates :username, :password, presence: true
+  validates :username, length: {in: 2..20},uniqueness: true
+  validates :password, on: :create, length: {in: 6..15}
 end
