6964e983774bdda126d0ea9b183bb421a444c3ca
Erin Hinson <hinson.erin@gmail.com>
2015-01-25 00:20:41 -0500

    fix user sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,25 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :require_same_user, only: [:edit, :update]
-  # GET /users
-  # GET /users.json
-  def index
-    @users = User.all
-  end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
   def edit
   end
 
-  # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to root_path, notice: 'User was successfully created.' }
+        session[:user_id] = @user.id
+
+        format.html { redirect_to root_path, notice: 'You are registered.' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
